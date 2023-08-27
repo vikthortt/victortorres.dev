39c0501685ee4666229debcbfa818253e9b64891
Victor Torres <vikthort.t@gmail.com>
2023-08-26 23:42:32 -0600

    Update styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://victortorres.dev'
+  site: 'https://victortorres.dev',
+  integrations: [vue()]
 });
