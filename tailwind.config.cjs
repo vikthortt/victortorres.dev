0bd9e930b42c3fa2f807e210a0d14b37b3e8cb93
Victor Torres <vikthort.t@gmail.com>
2024-01-21 17:23:51 -0600

    Update Astro and update text style

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,17 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
+		fontSize: {
+			sm: '0.8rem',
+			xl: '1.25rem',
+			'2xl': '1.563rem',
+			'3xl': '1.953rem',
+			'4xl': '2.441rem',
+			'5xl': '3.052rem',
+			base: '1.25rem',
+			title: '2.5rem',
+			section: '1.75rem'
+		},
 		fontFamily: {
 			header: ['"Karla Variable"', ...defaultTheme.fontFamily.sans],
 			body: ['"Nunito Variable"', ...defaultTheme.fontFamily.sans],
