0b071419feb12a67bb987481412539bed67457b4
Victor Torres <vikthort.t@gmail.com>
2025-07-22 01:25:09 -0600

    refactor: update Astro configuration and dependencies, integrate Tailwind CSS with Vite and config custom theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from 'astro/config';
-import vue from "@astrojs/vue";
 
-import tailwind from "@astrojs/tailwind";
+import vue from '@astrojs/vue';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  // integrations: [vue(), tailwind()]
   site: 'https://victortorres.dev',
-  integrations: [vue(), tailwind()]
+
+  integrations: [vue()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
