c27d21504f9a3b61ad9af3ad9cc917f373326313
Victor Torres <vikthort.t@gmail.com>
2025-07-22 17:26:47 -0600

    refactor: remove empty lines and commented-out code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,12 @@
-import { defineConfig } from 'astro/config';
-
-import vue from '@astrojs/vue';
-
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config'
+import vue from '@astrojs/vue'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-  // integrations: [vue(), tailwind()]
   site: 'https://victortorres.dev',
-
   integrations: [vue()],
-
   vite: {
     plugins: [tailwindcss()]
   }
-});
+})
